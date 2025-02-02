7e548ed18c82c603a1793eb382bf1aa757310b34
AKILSURYA SIVAKUMAR <64367796+AkilsuryaS@users.noreply.github.com>
2025-02-02 11:55:26 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def load_knowledge_base():
 def call_groq_api(prompt, simplify=False, concise=False):
     try:
         if simplify:
-            prompt = f"Explain the following in a very simple and easy-to-understand way in 5-6 sentences. If you don't know the answer, just say you don't know. Don't makeup the answers: {prompt}"
+            prompt = f"Explain the following in a very simple and easy-to-understand way in 5-6 sentences: {prompt}"
         elif concise:
-            prompt = f"Provide a concise and crisp answer to the following in 5-6 sentences.If you don't know the answer, just say you don't know. Don't makeup the answers: {prompt}"
+            prompt = f"Provide a concise and crisp answer to the following in 5-6 sentences: {prompt}"
         response = client.chat.completions.create(
             model="deepseek-r1-distill-llama-70b",
             messages=[{"role": "user", "content": prompt}],
